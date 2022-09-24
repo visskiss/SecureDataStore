93447e3b8046c078b6202bf0de4ea00b356db62c
Daniel Kanaan <dgk@danielkanaan.com>
2022-09-23 15:59:30 -1000

    Name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "FileHelper",
+    name: "SecureDataStore",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "FileHelper",
-            targets: ["FileHelper"]),
+            name: "SecureDataStore",
+            targets: ["SecureDataStore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "FileHelper",
+            name: "SecureDataStore",
             dependencies: []),
         .testTarget(
-            name: "FileHelperTests",
-            dependencies: ["FileHelper"]),
+            name: "SecureDataStoreTests",
+            dependencies: ["SecureDataStore"]),
     ]
 )
